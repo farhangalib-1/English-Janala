a49832d7398aa42e35fe24d8a452b6a1af8065d4
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-03 20:39:06 +0600

    Level buttons and Level words are added

---
+++ script.js
@@ -0,0 +1,61 @@
+const loadLevel = ()=>{
+    fetch('https://openapi.programming-hero.com/api/levels/all')
+    .then(req=>req.json())
+    .then(level=>displaylevel(level.data)
+    )
+}
+const displaylevel = (lessons)=>{
+    for(let lesson of lessons){
+        console.log(lesson);
+        const allLessons = document.getElementById('all-lessons')
+        const btn = document.createElement('button')
+        btn.innerHTML = `
+        <button class="btn btn-outline btn-primary" onclick = "word(${lesson.level_no})"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>
+        `
+        allLessons.appendChild(btn)
+    }
+    
+    };
+
+const word = (levelNo)=>{
+    const url = `https://openapi.programming-hero.com/api/level/${levelNo}`
+    fetch(url)
+    .then(req=>req.json())
+    .then(data=>displayWord(data.data))
+
+    const displayWord=(words)=>{
+        const displayWords = document.getElementById('lesson-display')
+        displayWords.innerHTML = ''
+    for(let word of words){
+    console.log(word)
+    const div = document.createElement('div')
+    div.innerHTML = `
+    <div class="words bg-white  m-4 rounded-xl py-14">
+            <div class="text-contents text-center">
+            <h1 class="font-bold text-[20px] mb-3">${word.word}</h1>
+            <p class="font-medium mb-6">Meaning /Pronounciation</p>
+            <p class="font-medium text-2xl hind-siliguri-medium">"${word.meaning} / ${word.pronunciation}"</p>
+            </div>
+            <div class="btns flex justify-between px-8 mt-6">
+              <button class="btn"><i class="fa-solid fa-circle-info"></i></button>
+              <button class="btn"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+          </div>
+    `
+        displayWords.appendChild(div)
+    }
+}
+}
+
+/*
+ 
+{id: 5,
+level: 1,
+word: 'Eager', 
+meaning: 'আগ্রহী', 
+: 'ইগার'}
+*/
+
+loadLevel();
+
+
