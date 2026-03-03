127fd15deda3cf96b506fa758796688a96464955
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-03 22:29:57 +0600

    button toggling is added

--- script.js
+++ script.js
@@ -6,28 +6,36 @@ const loadLevel = ()=>{
 }
 const displaylevel = (lessons)=>{
     for(let lesson of lessons){
-        console.log(lesson);
         const allLessons = document.getElementById('all-lessons')
         const btn = document.createElement('button')
         btn.innerHTML = `
-        <button class="btn btn-outline btn-primary" onclick = "word(${lesson.level_no})"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>
+        <button id="btn-${lesson.level_no}" class="lesson-btn btn btn-outline btn-primary" onclick = "word(${lesson.level_no})"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>
         `
         allLessons.appendChild(btn)
     }
-    
     };
+
+
 
 const word = (levelNo)=>{
     const url = `https://openapi.programming-hero.com/api/level/${levelNo}`
     fetch(url)
     .then(req=>req.json())
-    .then(data=>displayWord(data.data))
-
+    .then(data=>{
+        const allBtn = document.getElementsByClassName('lesson-btn')
+        for(let btn of allBtn){  
+            btn.classList.remove('active')
+        }
+        
+        const clickedBtn = document.getElementById(`btn-${levelNo}`)
+        clickedBtn.classList.add('active')
+        
+        displayWord(data.data)
+    })
     const displayWord=(words)=>{
         const displayWords = document.getElementById('lesson-display')
         displayWords.innerHTML = ''
     for(let word of words){
-    console.log(word)
     const div = document.createElement('div')
     div.innerHTML = `
     <div class="words bg-white  m-4 rounded-xl py-14">
@@ -44,6 +52,7 @@ const word = (levelNo)=>{
     `
         displayWords.appendChild(div)
     }
+
 }
 }
 
