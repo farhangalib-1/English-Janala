7ca719c02efd99a635d83860deffae3d169f1c9c
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-04 20:29:55 +0600

    Search Bar is added

--- script.js
+++ script.js
@@ -29,6 +29,46 @@ const displaylevel = (lessons)=>{
         allLessons.appendChild(btn)
     }
     };
+
+const displayWord=(words)=>{
+    const displayWords = document.getElementById('lesson-display')
+    displayWords.innerHTML = ''
+     if(words.length === 0){
+        displayWords.innerHTML = `
+        <div class="default-text col-span-full text-center py-16">
+        <div class="alertImage flex justify-center"><img src="images/alert-error.png" alt=""></div>
+
+    <p class="text-[13px] hind-siliguri-medium text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="text-[35px] hind-siliguri-medium">নেক্সট Lesson এ যান</h2>
+      </div>
+      `
+    }
+for(let word of words){
+    if(word.meaning === null || word.meaning === undefined){
+        word.meaning = 'কোন অর্থ পাওয়া যায়নি'
+    }
+    if(word.pronunciation === null || word.pronunciation === undefined){
+        word.pronunciation = 'কোন উচ্চারণ পাওয়া যায়নি'
+    }
+    
+const div = document.createElement('div')
+div.innerHTML = `
+<div class="words bg-white  m-4 rounded-xl py-14">
+        <div class="text-contents text-center">
+        <h1 class="font-bold text-[20px] mb-3">${word.word}</h1>
+        <p class="font-medium mb-6">Meaning /Pronounciation</p>
+        <p class="font-medium text-2xl hind-siliguri-medium">"${word.meaning} / ${word.pronunciation}"</p>
+        </div>
+        <div class="btns flex justify-between px-8 mt-6">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]" onclick="showModal(${word.id})"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]" onclick="pronounceWord(${word.id})"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
+`
+    displayWords.appendChild(div)
+}
+}
+
 const word = (levelNo)=>{
     const url = `https://openapi.programming-hero.com/api/level/${levelNo}`
     fetch(url)
@@ -42,45 +82,8 @@ const word = (levelNo)=>{
         clickedBtn.classList.add('active')
         displayWord(data.data) 
     })
-    const displayWord=(words)=>{
-        const displayWords = document.getElementById('lesson-display')
-        displayWords.innerHTML = ''
-         if(words.length === 0){
-            displayWords.innerHTML = `
-            <div class="default-text col-span-full text-center py-16">
-            <div class="alertImage flex justify-center"><img src="images/alert-error.png" alt=""></div>
+}
 
-        <p class="text-[13px] hind-siliguri-medium text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
-          <h2 class="text-[35px] hind-siliguri-medium">নেক্সট Lesson এ যান</h2>
-          </div>
-          `
-        }
-    for(let word of words){
-        if(word.meaning === null || word.meaning === undefined){
-            word.meaning = 'কোন অর্থ পাওয়া যায়নি'
-        }
-        if(word.pronunciation === null || word.pronunciation === undefined){
-            word.pronunciation = 'কোন উচ্চারণ পাওয়া যায়নি'
-        }
-        
-    const div = document.createElement('div')
-    div.innerHTML = `
-    <div class="words bg-white  m-4 rounded-xl py-14">
-            <div class="text-contents text-center">
-            <h1 class="font-bold text-[20px] mb-3">${word.word}</h1>
-            <p class="font-medium mb-6">Meaning /Pronounciation</p>
-            <p class="font-medium text-2xl hind-siliguri-medium">"${word.meaning} / ${word.pronunciation}"</p>
-            </div>
-            <div class="btns flex justify-between px-8 mt-6">
-              <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]" onclick="showModal(${word.id})"><i class="fa-solid fa-circle-info"></i></button>
-              <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]" onclick="pronounceWord(${word.id})"><i class="fa-solid fa-volume-high"></i></button>
-            </div>
-          </div>
-    `
-        displayWords.appendChild(div)
-    }
-}
-}
   const showModal = (id)=>{
     const url = `https://openapi.programming-hero.com/api/word/${id}`
     fetch(url)
@@ -125,13 +128,12 @@ const pronounceWord = (id)=>{
             const allWord = data.data
             const searchWord = allWord.filter(word =>
              word.word.toLowerCase().includes(searchTerm));
-                
-                
+            
             displayWord(searchWord); 
         })
         
     });
-
+  
 loadLevel();
 
 
